f2e9d4754db5dc8d96875f83047c80e86aba33a5
Karine Tavares <karinetavaresds12@gmail.com>
2022-05-05 19:52:47 -0300

    first commit

---
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { AnyForUntypedForms } from '@angular/forms';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.css']
+})
+export class MovimentacaoListComponent implements OnInit {
+
+  movimentacoes: any;
+  correntista:any;
+  correntistas:any = {};
+
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService
+    ) { }
+
+  ngOnInit(): void {
+    this.exibirCorrentistas();
+  }
+  listMovimentacoes(): void {
+    this.movimentacaoService.findByIdConta(this.correntista.id)
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+
+}
+}
